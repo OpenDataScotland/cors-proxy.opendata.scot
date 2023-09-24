0bd933346ab570634c3deae88b3929475ae0ee33
Jack Gilmore <46202639+JackGilmore@users.noreply.github.com>
2023-09-24 12:02:48 +0100

    CORS proxy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,56 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+const LOOPBACK_REGEX = /^(https?:\/\/)?(localhost|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(:\d{1,5})?$/;
+const WHITELISTED_SITE = "https://opendata.scot";
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+function isOriginAllowed(origin){
+	var regexMatch = LOOPBACK_REGEX.test(origin);
+	return origin == WHITELISTED_SITE || regexMatch;
+}
+
+// forked from: https://github.com/chebyrash/cors
+addEventListener("fetch", event => {
+	event.respondWith(handleRequest(event.request))
+  })
+  
+  async function handleRequest(request) {
+
+	var origin = request.headers.get("Origin");
+	var originIsAllowed = origin != null && isOriginAllowed(origin);
+
+	if (!originIsAllowed){
+		return new Response("Invalid origin", {status: 401});
+	}
+
+	try {
+	  const url = new URL(request.url);
+  
+	  if (url.pathname === "/") {
+		return new Response(`{"usage": "${url.origin}/<url>"}`);
+	  }
+  
+	  function addHeaders(response) {
+		response.headers.set("Access-Control-Allow-Origin", origin);
+		response.headers.set("Access-Control-Allow-Credentials", "true");
+		response.headers.set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+		response.headers.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	  }
+	  
+	  let response;
+	  if (request.method == "OPTIONS") {
+		response = new Response("");
+		addHeaders(response);
+		return response;
+	  }
+  
+	  response = await fetch(request.url.slice(url.origin.length + 1), {
+		  method: request.method,
+		  headers: request.headers,
+		  redirect: "follow",
+		  body: request.body
+	  });
+	  response = new Response(response.body, response)
+	  addHeaders(response);
+	  return response;
+	} catch (e) {
+	  return new Response(e.stack || e, {status: 500});
+	}
+  }
